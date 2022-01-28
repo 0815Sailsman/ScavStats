bc49f74b46fbfe3b5762a243cf32fcf7241d0226
Sailsman <danielgluch33@gmail.com>
2022-01-28 08:41:20 +0100

    Found lib for screenshots

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Nothing to see here
 # Main app will be launched from here etc.
 
-# TODO Add class that is responsible for the actual screen-capturing and that is able to supply that feed to other services
+# TODO Add class that is responsible for the actual screen-capturing
+#  and that is able to supply that feed to other services. Use MSS Library!
+#  https://python-mss.readthedocs.io/examples.html#part-of-the-screen-of-the-2nd-monitor
